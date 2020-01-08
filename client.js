1ed30b5f2124d9590ca830abd65151cbb44a5cd1
Conor Scott <conor.r.scott.88@gmail.com>
2020-01-08 11:23:08 +0000

    Add high rate client link

--- client.js
+++ client.js
@@ -1,15 +1,16 @@
 const WebSocket = require('ws')
 const url = 'ws://gold-price.cachetoken.io'
+//const url = 'ws://gold-price-2.cachetoken.io'
 const connection = new WebSocket(url)
- 
+
 connection.onopen = () => {
-  connection.send('Message From Client') 
+  connection.send('Message From Client')
 }
- 
+
 connection.onerror = (error) => {
   console.log(`WebSocket error: ${error}`)
 }
- 
+
 connection.onmessage = (e) => {
   console.log(e.data)
 }
